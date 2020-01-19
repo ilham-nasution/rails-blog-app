33ee91006a3e9d68378cc5b6b32ee8bfa3d4a38e
Ilham Rahman <ilhaamrahman@gmail.com>
2020-01-19 14:18:26 +0700

    blog post all action finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :destroy, :edit, :update]
+
     def index
         @posts = Post.all.order("created_at DESC")
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def new
@@ -15,15 +16,36 @@ class PostsController < ApplicationController
         @post = Post.new(post_params)
 
         if @post.save
-            redirect_to index
+            redirect_to root_path
         else
             render 'new'
         end
+    end
+
+    def edit
+    end
+
+    def update
+        if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+    end
+
+    def destroy
+        @post.destroy
+
+        redirect_to root_path
     end
 
     private
 
     def post_params
         params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+        @post = Post.find(params[:id])
     end
 end
